9bf42ada9b414688e61166026829e0d52c234371
beMimg <bernardommguerreiro@gmail.com>
2024-05-15 19:56:07 +0200

    Remove unsed import

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -3,15 +3,14 @@ import meetingImg from "../assets/nosotros-meeting.jpg";
 
 const Nosotros = () => {
   return (
-    // <img
-    //   src={meetingImg}
-    //   alt=""
-    //   className={`absolute left-0 top-0 -z-10 h-screen w-full   object-cover object-center`}
-    //   style={{
-    //     filter: "brightness(0.2)",
-    //   }}
-    // />
-    <p>loldd</p>
+    <img
+      src={meetingImg}
+      alt=""
+      className={`absolute left-0 top-0 -z-10 h-screen w-full   object-cover object-center`}
+      style={{
+        filter: "brightness(0.2)",
+      }}
+    />
   );
 };
 
