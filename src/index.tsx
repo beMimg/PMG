ca31e432f0b95cc676ed9488a8c7ce06e0117481
beMimg <bernardommguerreiro@gmail.com>
2024-05-15 13:57:38 +0200

    Delete unused components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import RouterComponent from "./Router";
 
 const root = ReactDOM.createRoot(
