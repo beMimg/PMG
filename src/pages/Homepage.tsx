9012fc51013a3fa0740124c4df5259058ff86f41
beMimg <bernardommguerreiro@gmail.com>
2024-05-16 15:09:07 +0200

    homepage screensize fix identation for small devices

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -14,18 +14,18 @@ const Homepage = () => {
             filter: "brightness(0.15)",
           }}
         />
-        <span className="logo-background-color h-[90%] w-[10px] rounded-md md:h-[75%]"></span>
+        <span className="logo-background-color  h-[75%] w-[10px] rounded-md"></span>
         <div className="flex h-full flex-col justify-evenly ">
-          <h2 className="  text-3xl font-bold md:text-5xl ">
+          <h2 className="  text-xl font-bold md:text-5xl ">
             Tu visión, nuestra misión: juntos, alcanzamos el éxito.
           </h2>
-          <h2 className="text-xl font-semibold opacity-90   md:text-3xl md:opacity-70">
+          <h2 className="text-lg font-semibold opacity-90   md:text-3xl md:opacity-70">
             Somos un equipo apasionado de expertos en consultoría y desarrollo
             web, comprometidos con la excelencia y la satisfacción del cliente.
           </h2>
           <NavLink
             to="/servicios"
-            className=" logo-background-color max-w-[300px] rounded-full border border-white p-1 px-5 text-center text-base font-semibold transition-all hover:border-teal-500 hover:bg-white hover:text-teal-500 md:border-2 md:text-lg"
+            className="logo-background-color self-start rounded-full border border-white p-1 px-14 text-center text-base font-semibold transition-all hover:border-teal-500 hover:bg-white hover:text-teal-500 md:border-2 md:text-lg"
           >
             Nuestros Servicios
           </NavLink>
