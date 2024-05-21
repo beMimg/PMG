2631bee1a25990b2a5b235f6f28487ce2b25c03d
beMimg <bernardommguerreiro@gmail.com>
2024-05-21 13:09:09 +0200

    Add style to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import logo from "../../assets/logo/logo_pmg2-removebg-preview.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className=" bg-slate-800 p-6 py-24 text-gray-400">
-      <div className="mx-auto flex max-w-7xl  flex-col justify-between gap-12 md:flex-row md:items-center">
-        <img src={logo} alt="PMG company" className="h-[200px] self-center" />
-        <div>
-          <p className="font-bold">Contáctanos</p>
-          <p>info@pmg-solutions.es</p>
+    <footer className="  bg-gray-950 p-6 py-14 ">
+      <div className="mx-auto flex max-w-7xl flex-col items-center gap-12 px-6 text-sm md:grid md:grid-cols-3">
+        <img src={logo} alt="PMG company" className=" w-[300px] self-center" />
+        <div className="flex h-full w-full flex-row items-start justify-evenly justify-self-center text-center text-gray-400 md:flex-col md:items-center ">
+          <div>
+            <p className="font-bold text-gray-200">Contáctanos</p>
+            <p>info@pmg-solutions.es</p>
+          </div>
+          <div>
+            <p className="font-bold text-gray-200">Visítanos</p>
+            <p>
+              C\ Plaza de España Nº 1, Planta 1, Oficina 5.
+              <br /> 11630 - Arcos de la Frontera.
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="font-bold">Visítanos</p>
-          <p>
-            C\ Plaza de España Nº 1, Planta 1, Oficina 5. 11630 - Arcos de la
-            Frontera.
-          </p>
+        <div className="flex h-full w-full flex-row justify-between justify-self-end font-semibold text-gray-200 md:w-min md:flex-col ">
+          <Link to="nosotros">Nosotros</Link>
+          <Link to="testimonios">Testimonios</Link>
+          <Link to="servicios">Servicios</Link>
+          <Link to="contacto">Contacto</Link>
+          <Link to="empleo">Empleo</Link>
         </div>
       </div>
     </footer>
