b6d428c63c52553ad7939283a6c2e3acac276bd7
beMimg <bernardommguerreiro@gmail.com>
2024-05-21 12:27:38 +0200

    Removed unused import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import RouterComponent from "./Router";
-import ScrollToTop from "./components/common/ScrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
