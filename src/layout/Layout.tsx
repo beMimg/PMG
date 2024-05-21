500c334a154ee5de1003bd82e204c8c58ae4e147
beMimg <bernardommguerreiro@gmail.com>
2024-05-21 11:25:34 +0200

    Create ScrollToTop and add it to top Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import ScrollToTop from "../components/common/ScrollToTop";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
