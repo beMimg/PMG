99ee4908ddc9ac0810574d05d18ec50eaeb6ee72
beMimg <bernardommguerreiro@gmail.com>
2024-05-22 13:45:17 +0200

    Add PorqueEligirnos to ServiciosSAP.tsx

--- src/components/servicios/ServicioSAP.tsx
+++ src/components/servicios/ServicioSAP.tsx
@@ -5,6 +5,7 @@ import especialistaSapImg from "../../assets/route-images/testimonios-meeting.jp
 import { negocios } from "../../components/servicios/CualEsNuestroNegocio";
 import ModeloDeTrabajo from "./ModeloDeTrabajo";
 import MetodologiasDeTrabajo from "./MetodologiasDeTrabajo";
+import PorqueEligirnos from "./PorqueEligirnos";
 
 const ServicioSAP = () => {
   return (
@@ -18,7 +19,7 @@ const ServicioSAP = () => {
         <div className="flex flex-col gap-24">
           <div className=" grid  gap-16  text-center md:grid-cols-3">
             {negocios.map((negocio) => (
-              <div className="blurred-bg relative rounded-lg transition-all duration-200  ease-in-out ">
+              <div className="blurred-bg relative rounded-lg transition-all duration-200 ease-in-out ">
                 <div className="flex h-full w-full flex-col gap-2 rounded-lg bg-gray-50 p-4 shadow-lg transition-all hover:scale-95 hover:shadow-none">
                   <h2 className="text-xl font-semibold">{negocio.title}</h2>
                   <p className="text-lg">{negocio.description}</p>
@@ -26,6 +27,7 @@ const ServicioSAP = () => {
               </div>
             ))}
           </div>
+          <PorqueEligirnos />
           <MetodologiasDeTrabajo />
         </div>
       </SectionMaxW7XL>
